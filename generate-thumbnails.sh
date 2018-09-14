8125952e041e22b968a2a30cbb7dcc93d498d37f
Ryan Moodie <ryanmoodie@gmail.com>
2018-09-14 15:13:49 +0100

    Tweak

--- generate-thumbnails.sh
+++ generate-thumbnails.sh
@@ -1,1 +1,13 @@
-for f in static/albums/home/*; do convert $f -thumbnail 640x640^\> static/thumbnails/home/${f%.jpg}-thumbnail.jpg; done
+#!/bin/bash
+
+# Accepts one argument, which should be the name of the album folder you want to generate thumbnails for
+# E.g. bash generate-thumbnails.sh home
+# Uses Imagemagick to create thumbnals for all the images in the album
+
+for file in static/albums/$1/*; 
+do 
+	temp=${file#static/albums}
+	thumb=static/thumbnails${temp%.jpg}-thumbnail.jpg
+	echo "$file -> $thumb";
+	convert $file -thumbnail 640x640^\> $thumb; 
+done
